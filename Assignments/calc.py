aec20df7352592a9506a6b1cdeab56d3e0b7a301
V-Kariuki <munenevq@gmail.com>
2023-09-28 21:53:42 +0300

    Initial commit

---
+++ Assignments/calc.py
@@ -0,0 +1,6 @@
+name= input("Please Enter your Name ")
+print("Welcome to my calculator", name, "\n What Numbers would you like to add?")
+Number1 = int (input("Please Enter the First Number "))
+Number2 = int (input("\n Please enter the second Number "))
+#sum = Number1 + Number2
+print (name, "The sum of ", Number1, " and ", Number2, "is ",Number1+Number2)
