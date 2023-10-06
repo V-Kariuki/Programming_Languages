ed83a91d20ffb67d881ff88443cd186f89c21a2f
V-Kariuki <munenevq@gmail.com>
2023-10-06 12:43:38 +0300

    Code update for the class held on 6th of October 2023

---
+++ Course Resources/Python/Code/code06th.py
@@ -0,0 +1,30 @@
+'''number1=0
+while number1<5:
+    print(number1)
+    number1=number1+1
+
+while expression:
+    while expression:
+        statements
+        statements
+    '''
+list1 = ["my", "class","and","school"]
+for word in list1:
+    print (word)
+
+fruits = {'apple':5,'mango': 10,'orange':20}
+for fruit in fruits.items():
+    print(fruit)
+
+'''for iterable in sequence:
+    for iterable1 in sequence:
+        statements
+        statements
+        '''
+plants = ["apple","mango","pear"]
+
+for plant in plants:
+    for letter in plant:
+        print (letter)
+        
+    print(plant)    
